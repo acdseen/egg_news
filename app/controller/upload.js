c95378ae1778322bd7556f7b057495a4dd2c64aa
zhonglinhong <zhonglinhong@567na.com>
2018-07-24 10:15:33 +0800

    图片上传

--- app/controller/upload.js
+++ app/controller/upload.js
@@ -1,9 +1,19 @@
-const BaseController=require('./base');
-class UploadController extends BaseController{
-    async uploadimg(){
-        let {ctx}=this;
-        let data=ctx.request.body;
-        console.log(data);
+const BaseController = require('./base');
+const pump = require('mz-modules/pump');
+const fs = require('fs');
+const path = require('path');
+
+class UploadController extends BaseController {
+    async uploadimg() {
+        const stream = await this.ctx.getFileStream();
+        this.success(stream);
+        //console.log(stream.fields.name);
+        const filename = encodeURIComponent(stream.fieldname) + path.extname(stream.fields.filename).toLowerCase();
+        const target = path.join(this.config.baseDir, 'app/public', filename);
+        const writeStream = fs.createWriteStream(target);
+        await pump(stream, writeStream);
+        this.ctx.redirect('/public/' + filename);
+        
     }
 };
-module.exports=UploadController;
+module.exports = UploadController;
